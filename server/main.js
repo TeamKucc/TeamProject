a5279b7efd19efc8756ec2d2bb999e9a6db16d88
93seungmi <65018851+93seungmi@users.noreply.github.com>
2020-07-23 09:34:04 +0900

    Update main.js

--- server/main.js
+++ server/main.js
@@ -2,12 +2,8 @@ require('dotenv').config();
 import express from 'express';
 
 import mongoose from 'mongoose';
-<<<<<<< HEAD
 import cookieParser from 'cookie-parser'
 import api from './api/index'
-=======
-import api from './api/index';
->>>>>>> 173e0f07595014dd2b535d0d6b2189995cf14739
 
 const app = express();
 const { PORT, MONGO_URI } = process.env;
@@ -21,19 +17,13 @@ mongoose.connect(
     } else {
       console.log('Error!:' + JSON.stringify(err, undefined, 2));
     }
-<<<<<<< HEAD
 )
 
 app.use(bodyparser.urlencoded({ extended: true }))
 app.use(bodyparser.json());
 app.use(cookieParser())
-=======
-  },
-);
->>>>>>> 173e0f07595014dd2b535d0d6b2189995cf14739
 
 app.use('/api', api);
-
 app.use('/uploads', express.static('uploads'));
 
 app.listen(PORT, () => console.log('Server ON:' + PORT));
