44e77d2ea7dc870c4f3858a28695e69c5a359faa
93seungmi <65018851+93seungmi@users.noreply.github.com>
2020-07-23 09:18:16 +0900

    Update index.js

--- server/api/index.js
+++ server/api/index.js
@@ -1,23 +1,14 @@
 import express from 'express';
-<<<<<<< HEAD
+
 import auth from './auth'
 import user from './user'
 import authCheck from '../lib/auth';
-=======
-import user from './user/index';
-import product from './product/index';
->>>>>>> 173e0f07595014dd2b535d0d6b2189995cf14739
+import product from './product';
 
 const router = express.Router();
 
-<<<<<<< HEAD
 router.use('/auth',auth);
 router.use('/user',user)
 router.use('/product', product);
-export default router;
-=======
-router.use('/user', user);
-router.use('/product', product);
 
 export default router;
->>>>>>> 173e0f07595014dd2b535d0d6b2189995cf14739
