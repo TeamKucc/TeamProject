ca7c3354399eafa4e09184fbfc617ef8f83a02e5
sunguekim <sunguekim96@gmail.com>
2020-07-24 17:42:05 +0900

    김선규/React Logout 모듈 완성\node admin 모듈생성 시작

--- server/api/user/user.ctrl.js
+++ server/api/user/user.ctrl.js
@@ -1,4 +1,4 @@
-import User from '../../models/user';
+import User from '../../models/user'
 
 export const list = (req, res) => {
     if (!req.decode.admin) {
@@ -39,20 +39,3 @@ export const userUpdate = (req,res)=>{
         })
     })
 }
-
-// export const assignAdmin = (req, res) => {
-//   if (!req.decode.admin) {
-//     return res.status(403).json({
-//       message: 'you are not adimn',
-//     });
-//   }
-
-//   User.findByUsername(req.params.username)
-//     .then((user) => user.assignAdmin())
-//     .then(
-//       res.json({
-//         success: true,
-//       }),
-//     );
-// };
-
