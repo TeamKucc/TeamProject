a00ee09066df02cad4fc8b49b73a93a919bdf5fe
93seungmi <93seungmi@gmail.com>
2020-07-29 09:10:35 +0900

    어성미 / 재고관리페이지 redux 구현중

--- front/src/lib/api/product.js
+++ front/src/lib/api/product.js
@@ -1,9 +1,6 @@
 import client from "./client"
 
 export const productUpload = ({ thumbnails, title, description, price, images, discount, person }) => client.post("/api/product/productUpload", { thumbnails, title, description, price, images, discount, person })
-<<<<<<< HEAD
 export const landingProduct = ({ thumbnails, title, price, discount }) => client.get('/api/product/productLanding', { thumbnails, title, price, discount })
 export const readProduct = ({_id})=>client.post('/api/product/readProduct',{_id})
-=======
-export const landingProduct = ({ thumbnails, title, price, discount }) => client.get('/api/product/getProducts', { thumbnails, title, price, discount })
->>>>>>> origin/master
+export const stockManagement = ({ stock, thumbnails, title, price, discount }) => client.get('/api/product/getStock', { stock, thumbnails, title, price, discount })
--- server/api/product/index.js
+++ server/api/product/index.js
@@ -8,7 +8,6 @@ router.post('/uploadImage', productCtrl.uploadImage);
 router.post('/uploadThumbnail', productCtrl.uploadThumbnail);
 router.post('/productUpload', productCtrl.productUpload);
 router.get('/getProducts', productCtrl.getProducts);
-router.post('/readProduct',productCtrl.readProduct);
-router.get('/config',productCtrl.config)
+router.get('/getStock', productCtrl.getStock)
 
 export default router;
