f1ecacc29906ba11cc725f028808fa0bdadce48c
93seungmi <93seungmi@gmail.com>
2020-07-29 09:55:58 +0900

    Revert "Revert "어성미 / 재고관리페이지 redux 구현중""
    This reverts commit bc83db585085c4634c75001d9ba1ac3f961bb9ce.

--- front/src/lib/api/product.js
+++ front/src/lib/api/product.js
@@ -2,4 +2,5 @@ import client from "./client"
 
 export const productUpload = ({ stock, thumbnails, title, description, price, images, discount, person }) => client.post("/api/product/productUpload", { stock, thumbnails, title, description, price, images, discount, person })
 export const landingProduct = ({ thumbnails, title, price, discount }) => client.get('/api/product/getProducts', { thumbnails, title, price, discount })
+export const readProduct = ({_id})=>client.post('/api/product/readProduct',{_id})
 export const stockManagement = ({ stock, thumbnails, title, price, discount }) => client.get('/api/product/getStock', { stock, thumbnails, title, price, discount })
