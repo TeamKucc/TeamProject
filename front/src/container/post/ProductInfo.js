2d9573b8d9496fefaaef459a1ff5d2f532a202d4
sunguekim <sunguekim96@gmail.com>
2020-07-31 09:08:44 +0900

    김선규/React 결제 예외처리

--- front/src/container/post/ProductInfo.js
+++ front/src/container/post/ProductInfo.js
@@ -21,13 +21,14 @@ const ProductInfo = ({ match, history }) => {
     }, [dispatch])
 
     const onClick = () => {
+        if(!user) return alert('권한이 없습니다');
         IMP.init('imp97305641');
         IMP.request_pay({
             pg: 'kakao', // version 1.1.0부터 지원.
             pay_method: 'card',
             merchant_uid: 'merchant_' + new Date().getTime(),
             name: '주문명:결제테스트',
-            amount: 14000,
+            amount: 1000,
             buyer_email: 'iamport@siot.do',
             buyer_name: '구매자이름',
             buyer_tel: '010-1234-5678',
@@ -40,6 +41,7 @@ const ProductInfo = ({ match, history }) => {
                 msg += '상점 거래ID : ' + rsp.merchant_uid;
                 msg += '결제 금액 : ' + rsp.paid_amount;
                 msg += '카드 승인번호 : ' + rsp.apply_num;
+                // window.location.reload()
             } else {
                 var msg = '결제에 실패하였습니다.';
                 msg += '에러내용 : ' + rsp.error_msg;
