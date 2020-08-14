e675a9be49fa17a58d99aae88401f8d47f5bc5a6
93seungmi <93seungmi@gmail.com>
2020-08-14 09:44:09 +0900

    어성미 / error fix

--- front/src/pages/UserInfo.js
+++ front/src/pages/UserInfo.js
@@ -1,18 +1,4 @@
 import React from 'react';
-<<<<<<< HEAD
-import UserInfoContainer from '../container/auth/UserInfoContainer'
-import HeaderContainer from "../container/common/HeaderContainer"
-
-const UserInfo =()=>{
-    return(
-        <div>
-            <HeaderContainer />
-            <UserInfoContainer />
-        </div>
-
-    )
-}
-=======
 import UserInfoContainer from '../container/auth/UserInfoContainer';
 import HeaderContainer from '../container/common/HeaderContainer';
 
@@ -24,6 +10,5 @@ const UserInfo = () => {
     </div>
   );
 };
->>>>>>> d77bda53db3ea71ac267af672b17152351fa0752
 
 export default UserInfo;
