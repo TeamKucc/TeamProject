ed779dd2367f3ec43c47446c9140060c83720cd7
93seungmi <93seungmi@gmail.com>
2020-08-30 02:54:06 +0900

    어성미 / 테스트 폴더 추가

---
+++ client/src/test/Test.js
@@ -0,0 +1,17 @@
+import PropTypes from "prop-types";
+import React, { useEffect, useState } from "react";
+import HeadTest from '../container/HeadTest'
+
+const Test = () => {
+
+  return (
+    <HeadTest
+      headerContainerClass="container-fluid"
+      headerPaddingClass="header-padding-1"
+    >
+    </HeadTest>
+  )
+};
+
+
+export default Test;
