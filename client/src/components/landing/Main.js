c93be7d026b7d967ee14666ab174df9642ed3981
sunguekim <sunguekim96@gmail.com>
2020-09-16 15:01:14 +0900

    김선규/Docker Set 수정

--- client/src/components/landing/Main.js
+++ client/src/components/landing/Main.js
@@ -137,7 +137,7 @@ const Main = ({ products }) => {
                           <img
                             className="default-img"
                             src={`${product.thumbnails[0].image.location}`}
-                            alt={`productImg-${index}`}
+                            alt="메인 이미지"
                           />
                         }
 
